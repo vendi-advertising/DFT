7995749d5237e18f7e6cffd7b0d9c620ee2cf16e
Chris Haas <chris@vendiadvertising.com>
2019-02-06 05:34:05 -0600

    Update tests for Complex\Complex

--- tests/DFTTest.php
+++ tests/DFTTest.php
@@ -2,52 +2,48 @@
 namespace StjepanBrbot;
 
 use StjepanBrbot\DFT;
-use StjepanBrbot\Complex;
+use Complex\Complex;
 
-function compare1DArrays($a,$b)
+function compare1DArrays(Iterable $a, Iterable $b) : bool
 {
-  foreach($a as $i => $val)
-  {
-    if(abs($val-$b[$i]) > 0.001) return false;
-  }
-  return true;
+    foreach($a as $i => $val){
+        if(abs($val - $b[$i]) > 0.001){
+            return false;
+        }
+    }
+
+    return true;
 }
 
-function compareComplexArrays($a,$b)
+function compareComplexArrays(Iterable $a, Iterable $b) : bool
 {
-  foreach($a as $i => $C)
-  {
-    if(abs($C->getRe()-$b[$i]->getRe())>0.001) return false;
-    if(abs($C->getIm()-$b[$i]->getIm())>0.001) return false;
-  }
-  return true;
+    foreach($a as $i => $C) {
+        if(abs($C->getReal() - $b[$i]->getReal()) > 0.001){
+            return false;
+        }
+        if(abs($C->getImaginary() - $b[$i]->getImaginary()) > 0.001){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 class DFTTest extends \PHPUnit_Framework_TestCase
 {
 
-  protected $DFT;
+    public function testGetFactors()
+    {
+        $DFT = new DFT([2,3,-1,1]);
+        $F = [new Complex(5,0), new Complex(3,-2), new Complex(-3,0), new Complex(3,2)];
+        $this->assertTrue(compareComplexArrays($F, $DFT->getFactors()));
+    }
 
-  protected function setUp()
-  {
-    $this->DFT=new DFT([2,3,-1,1]);
-  }
-
-  protected function tearDown()
-  {
-
-  }
-
-  public function testGetFactors()
-  {
-    $F=[new Complex(5,0),new Complex(3,-2),new Complex(-3,0),new Complex(3,2)];
-    $this->assertTrue(compareComplexArrays($F,$this->DFT->getFactors()));
-  }
-
-  public function testGetSpectra()
-  {
-    $S=[5,3.606,3,3.606];
-    $this->assertTrue(compare1DArrays($S,$this->DFT->getSpectra()));
-  }
+    public function testGetSpectra()
+    {
+        $DFT = new DFT([2,3,-1,1]);
+        $S = [5, 3.606, 3, 3.606];
+        $this->assertTrue(compare1DArrays($S, $DFT->getSpectra()));
+    }
 
 }
